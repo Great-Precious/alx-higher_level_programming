e75ae0a7e1d1918bee694543b684c1cd4763a8e1
Great-Precious <chrisprecious05@gmail.com>
2023-08-16 16:26:25 -0700

    103-python.c

---
+++ 0x04-python-more_data_structures/list.h
@@ -0,0 +1,11 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <python.h>
+
+void print_python_list(PyObject *p);
+/usr/include/python3.4/bytesobject.h
+
+#endif LISTS_H
